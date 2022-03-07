e72434ed8dda6664e28a2cf8427b0f6a1133a703
bdcrisco <65649340+bdcrisco@users.noreply.github.com>
2022-03-07 15:48:39 -0700

    changed stuff

--- testforgit.py
+++ testforgit.py
@@ -11,15 +11,18 @@ class Person:
 
 class Height:
     def __init__(self):
-        self.feet = 4
-        self.inches = 5
+        self.feet = 7
+        self.inches = 11
     def __init__(self,feet,inches=0):
         self.feet = feet
         self.inches = inches
     def display(self):
         print(f"{self.feet}'{self.inches}\"")
 person1 = Person("Greg", Height(2,11))
-person2 = Person("Terry", Height(8))
+person4 = Person("John", Height(5))
+person2 = Person("Terry", Height(6))
 person1.display()
 person2.display()
 
+person3 = Person("Chris", Height(6,2))
+person3.display()
