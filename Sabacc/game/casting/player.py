2661f87b31f8636a7bf018fc54f919134bf7c7cb
Brandon Van Blarcum <41463636+Drofal980@users.noreply.github.com>
2022-04-06 18:12:53 -0600

    starting to implement chips

--- Sabacc/game/casting/player.py
+++ Sabacc/game/casting/player.py
@@ -8,17 +8,27 @@ class Player():
         is_playing (boolean): Whether or not the game is being played.
     """
 
-    def __init__(self, hand = Deck()):
+    def __init__(self, chips = 0, hand = Deck()):
         """Constructs a new instance of Player
         
         Args:
             self (Player): An instance of Player
         """
         self._hand = hand
+        self._chips = chips
 
     def get_hand(self):
         return self._hand
     
     def add_card(self, card):
         self._hand.add(card)
+    
+    def get_chips(self):
+        return self._chips
+
+    def add_chips(self, amount = int()):
+        self._chips += amount
+
+    def subtract_chips(self, amount = int()):
+        self._chips -= amount
 
